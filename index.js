d44e25bef4dfed77cc2b90165861f506ad1bfc47
MateusSilvaAragao <aragaosm@icloud.com>
2022-01-11 21:13:12 -0500

    commiting to the main

---
+++ index.js
@@ -0,0 +1,15 @@
+// comments that i can write this way of coding 
+//i can type without looking at the keyboard
+// java script use the camel notation 
+let name = 'Mateus';
+function greet(name){
+    console.log('hello world '+ name ); 
+}
+
+greet("john");
+
+function square(number){
+    return number*number;
+}
+console.log(square(2));
+console.log("my name is mateus")
